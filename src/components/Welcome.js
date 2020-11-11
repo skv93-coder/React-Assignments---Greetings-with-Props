6a5222d36ae35fc28d1dbd0b7ee5182933176766
skv93-coder <68867418+skv93-coder@users.noreply.github.com>
2020-11-11 20:00:10 +0530

    change

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,7 +3,7 @@ export default function (props) {
   const { name } = props;
   return (
     <>
-      <h1>Hey ! {name}</h1>
+      <h1>Hey {name} !</h1>
       <h2>Welcome to Newton School.</h2>
     </>
   );
